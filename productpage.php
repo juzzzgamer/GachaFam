80cf6ea360cbcaa4683297e1ae58bf569f34b4af
Yen Ming Jun <mikeyenmingjun@gmail.com>
2024-05-27 04:39:53 +0800

    modified and improve the code

--- productpage.php
+++ productpage.php
@@ -1,24 +1,33 @@
+<?php
+include("dbh.inc.php");
+include("session.php");
+try {
+    $stmt = $pdo->prepare("SELECT product_name, listing_desc, img, price FROM listings");
+    $stmt->execute();
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Query failed: " . $e->getMessage());
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <title>box</title>
 <link rel="stylesheet" href="product_page.css">
 <link rel="stylesheet" href="style.css">
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/mike
 </head>
 <body>
     <div class="menu_bar">
         <h1 class="logo">Gacha<span>Fam.</span></a></h1>
         <ul>
-            <li><a href="file:///C:/Users/Yen%20Ming%20Jun/OneDrive/Desktop/mini%20it%20project.html/cases.html">Cases</a></li>
-            <li><a href="file:///C:/Users/Yen%20Ming%20Jun/OneDrive/Desktop/mini%20it%20project.html/cart.html">Cart</a></li>
-            <li><a href="file:///C:/Users/Yen%20Ming%20Jun/OneDrive/Desktop/New%20folder/WebApp/login.html">Account</a></li>
+        <li><a href="#" id="profile">Welcome, <span style="color:red"><?php echo ("$username")?></span></a></li>
+            <li><a href="create.php">Create listing</a></li>
+            <li><a href="cases.html">Cases</a></li>
+            <li><a href="cart.html">Cart</a></li>
+            <li><a href="logout.php">Logout</a></li>
         </ul>
     </div>
-<<<<<<< HEAD
     <div class="container">
         <div class="col-1">
             <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="box" srcset="">
@@ -45,77 +54,22 @@
                     </div>
                 </div>
             </div>
-            <div class="product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+            <div class="product_img-container">
+                <div class="product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                    <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                </div>
             </div>
         </div>
     </div>
     <script src="productpage.js"></script>
-=======
-    <section class="box">
-        <h2>box</h2>
-        <div class="image">
-            <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product 1">
-        </div>
-        <div class="detail">
-            <h4>$$$</h4>
-        </div>
-        <div class="page_case_open">
-            <div class="count-list">
-                <span class="minus">-</span>
-                <span class="num">01</span>
-                <span class="plus">+</span>
-            </div>
-            <div class="purchase">
-                <button id="purchase">buy</button>
-            </div>
-        </div>
-    </section>
-    <script>
-
-        const plus = document.querySelector(".plus"),
-        minus = document.querySelector(".minus"),
-        num = document.querySelector(".num");
-
-        let a = 1;
-
-        plus.addEventListener('click',()=>{
-            a++;
-            a = (a<10) ? "0" + a : a;
-            num.innerText = a;
-            console.log(a);
-        });
-
-        minus.addEventListener('click',()=>{
-            if(a>1){
-                a--;
-                a = (a<10) ? "0" + a : a;
-                num.innerText=a;
-            }
-        });
-    </script>
-    <div class="box_contain"><h2>Box Contains</h2></div>
-    <div class="product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-        <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
-    </div>
->>>>>>> origin/mike
 </body>
 </html>
