3a89d78467ae3d2b396a4f29f41bf8362235c166
Yen Ming Jun <mikeyenmingjun@gmail.com>
2024-05-26 14:47:19 +0800

    modified productpage.php and added productpage.js

---
+++ productpage.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>box</title>
+<link rel="stylesheet" href="product_page.css">
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="menu_bar">
+        <h1 class="logo">Gacha<span>Fam.</span></a></h1>
+        <ul>
+            <li><a href="file:///C:/Users/Yen%20Ming%20Jun/OneDrive/Desktop/mini%20it%20project.html/cases.html">Cases</a></li>
+            <li><a href="file:///C:/Users/Yen%20Ming%20Jun/OneDrive/Desktop/mini%20it%20project.html/cart.html">Cart</a></li>
+            <li><a href="file:///C:/Users/Yen%20Ming%20Jun/OneDrive/Desktop/New%20folder/WebApp/login.html">Account</a></li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="col-1">
+            <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="box" srcset="">
+        </div>
+        <div class="col-2">
+            <div class="product-container">
+                <p class="product-name">box</p>
+                <p class="price">Price 5</p>
+                <div class="btn">
+                    <button class="btn1 btn-decrement">-</button>
+                    <input type="text" id="quantity" class="btn-input" value="1">
+                    <button class="btn1 btn-increment">+</button>
+                    <div class="purchase">
+                        <button id="purchase">buy</button>
+                    </div>
+
+                    <div class="total-amount">
+                        <h1>total-amount</h1>
+                        <p class="total-price">
+                            <span id="price">
+                                5 
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+                <img src="https://egamersworld.com/uploads/blog/16655771848831.jpg" alt="Product_img">
+            </div>
+        </div>
+    </div>
+    <script src="productpage.js"></script>
+</body>
+</html>
