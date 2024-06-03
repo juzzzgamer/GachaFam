552838390e3c23cfebc3af89c43e8f5e38b0922d
Yen Ming Jun <mikeyenmingjun@gmail.com>
2024-06-03 20:36:13 +0800

    Committing local changes before pulling from probability branch

--- create.php
+++ create.php
@@ -38,7 +38,7 @@ try {
 </head>
 <body>
     <div class="menu_bar">
-        <h1 class="logo">Gacha<span>Fam.</span></h1>
+        <a href="index.php" class="logo"><h3>Gacha<span>Fam.</span></h3></a>
         <ul>
             <li><a href="#" id="profile">Welcome, <span style="color:red"><?php echo ($username); ?></span></a></li>
             <li><a href="create.php">Create listing</a></li>
@@ -46,81 +46,81 @@ try {
             <li><a href="cart.html">Cart</a></li>
             <li><a href="#">Profile</a></li>
         </ul>
-    </div>      
-    <div class="container">
-        <form id="createForm" action="formhandler.inc.php" method="post" enctype="multipart/form-data">
-            <div id="initialFields">
-                <h1>Create Listing</h1>
-                <div class="entryarea">
-                    <input type="text" name="Name" placeholder="Name of product" required>
+    </div>
+    <div class="page-container">
+        <div class="container">
+            <form id="createForm" action="formhandler.inc.php" method="post" enctype="multipart/form-data">
+                <div id="initialFields">
+                    <h1>Create Listing</h1>
+                    <div class="entryarea">
+                        <input type="text" name="Name" placeholder="Name of product" required>
+                    </div>
+                    <div class="entryarea">
+                        <input type="file" name="img" accept=".jpg,.jpeg,.png" placeholder="Image" required>
+                    </div>
+                    <div class="entryarea">
+                        <input type="text" name="desc" placeholder="Description" required>
+                    </div>
+                    <div class="entryarea">
+                        <input type="number" name="price" placeholder="Price" required>
+                    </div>
+                    <button type="submit" name="createGame">Submit</button>
+                    <div class="additional-options">
+                        <p>Willing to upload items? <a href="#" onclick="showItemForm()">Items</a></p>
+                        <p>Willing to update stock? <a href="#" onclick="showStockForm()">Update stock</a></p>
+                    </div>
                 </div>
-                <div class="entryarea">
-                    <input type="file" name="img" accept=".jpg,.jpeg,.png" placeholder="Image" required>
-                </div>
-                <div class="entryarea">
-                    <input type="text" name="desc" placeholder="Description" required>
-                </div>
-                <div class="entryarea">
-                    <input type="number" name="price" placeholder="Price" required>
-                </div>
-                <button type="submit" name="createGame">Submit</button>
-                <div class="updatestock">
-                    <p>Willing to upload items? <a href="#" onclick="showItemForm()">Items</a></p>
-                </div>
-                <div class="updatestock">
-                    <p>Willing to update stock? <a href="#" onclick="showStockForm()">Update stock</a></p>
-                </div>
-            </div>
-        </form>
-        <form id="createForm" action="formhandler.inc.php" method="post" enctype="multipart/form-data">
-            <div id="itemFields" style="display:none;">
-                <h2>Items</h2>
-                <div class="entryarea">
-                    <input type="text" name="itemsName[]" placeholder="Item" required>
-                    <input type="file" name="itemsImg[]" accept=".jpg,.jpeg,.png" required>
-                </div>
-                <button type="submit" name="itemsUpload">Submit</button>
-                <div class="updatestock">
-                    <p>Willing to create game? <a href="#" onclick="showGameForm()">Create game</a></p>
-                </div>
-                <div class="updatestock">
-                    <p>Willing to update stock? <a href="#" onclick="showStockForm()">Update stock</a></p>
-                </div>
-            </div>
-        </form>
-        <form id="createForm" action="formhandler.inc.php" method="post" enctype="multipart/form-data">
-            <div id="stockDisplay" style="display:none;">
-                <?php foreach ($items as $item): ?>
-                    <h3><?php echo htmlspecialchars($item['name']); ?></h3>
-                    <img src="upload/<?php echo htmlspecialchars($item['img']); ?>" alt="<?php echo htmlspecialchars($item['name']); ?>">
-                    <p><?php echo htmlspecialchars($item['stock']); ?></p>
-                <?php endforeach; ?>
-                <h1>Stock Update</h1>
-                <div class="entryarea">
-                    <label for="item_id_update">Select a product:</label>
-                    <select id="item_id_update" name="id">
-                    <?php 
-                    foreach ($items as $item) {
-                        echo "<option value=\"{$item['id']}\">{$item['name']}</option>";
-                    }
-                    ?>
-                    </select>
-                </div>
-                <br><br>
-                <div class="entryarea">
-                    <label for="quantity">Enter quantity:</label>
-                    <input type="number" id="quantity" name="quantity">
-                </div>
-                <br><br>
-                <button type="submit" name="stockUpdate">Submit</button>
-                <div class="updatestock">
-                    <p>Willing to create game? <a href="#" onclick="showGameForm()">Create game</a></p>
-                </div>
-                <div class="updatestock">
-                    <p>Willing to upload items? <a href="#" onclick="showItemForm()">Items</a></p>
-                </div>
-            </div>
             </form>
+            <form id="createForm" action="formhandler.inc.php" method="post" enctype="multipart/form-data">
+                <div id="itemFields" style="display:none;">
+                    <h2>Items</h2>
+                    <div class="entryarea">
+                        <input type="text" name="itemsName[]" placeholder="Item" required>
+                        <input type="file" name="itemsImg[]" accept=".jpg,.jpeg,.png" required>
+                    </div>
+                    <button type="submit" name="itemsUpload">Submit</button>
+                    <div class="additional-options">
+                        <p>Willing to create game? <a href="#" onclick="showGameForm()">Create game</a></p>
+                        <p>Willing to update stock? <a href="#" onclick="showStockForm()">Update stock</a></p>
+                    </div>
+                </div>
+            </form>
+            <form id="createForm" action="formhandler.inc.php" method="post" enctype="multipart/form-data">
+                <div id="stockDisplay" style="display:none;">
+                    <h1>Stock Update</h1>
+                    <div class="entryarea">
+                        <label for="item_id_update">Select a product:</label>
+                        <select id="item_id_update" name="id">
+                        <?php 
+                        foreach ($items as $item) {
+                            echo "<option value=\"{$item['id']}\">{$item['name']}</option>";
+                        }
+                        ?>
+                        </select>
+                    </div>
+                    <br><br>
+                    <div class="entryarea">
+                        <label for="quantity">Enter quantity:</label>
+                        <input type="number" id="quantity" name="quantity">
+                    </div>
+                    <br><br>
+                    <button type="submit" name="stockUpdate">Submit</button>
+                    <div class="additional-options">
+                        <p>Willing to create game? <a href="#" onclick="showGameForm()">Create game</a></p>
+                        <p>Willing to upload items? <a href="#" onclick="showItemForm()">Items</a></p>
+                    </div>
+                    <div class="item-row">
+                        <?php foreach ($items as $item): ?>
+                        <div class="item-detail">
+                            <h3><?php echo htmlspecialchars($item['name']); ?></h3>
+                            <img src="upload/<?php echo htmlspecialchars($item['img']); ?>" alt="<?php echo htmlspecialchars($item['name']); ?>">
+                            <p><?php echo htmlspecialchars($item['stock']); ?></p>
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                </form>
+        </div>
     </div>
 </body>
 </html>
