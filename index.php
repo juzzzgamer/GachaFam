3a358e9ef235a7f9b98a5ab2cf5f27806841b6ea
ethan <ethanchong567@gmail.com>
2024-06-09 23:33:43 +0800

    display result in gacha.php (winnerpage) and added credit system

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 include("dbh.inc.php");
 include("session.php");
+
+
+$userId = $_SESSION['user_id'];
+
 try {
-    $stmt = $pdo->prepare("SELECT id, game_name, game_desc, img, price FROM game");
+  
+    $stmt = $pdo->prepare("SELECT credits FROM user WHERE id = ?");
+    $stmt->execute([$userId]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $userCredits = $user ? $user['credits'] : 0;
+
+    $stmt = $pdo->prepare("
+        SELECT game.id, game.game_name, game.game_desc, game.img, game.price, user.credits 
+        FROM game
+        LEFT JOIN user ON game.user_id = user.id
+    ");
     $stmt->execute();
     $games = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
@@ -23,7 +37,9 @@ try {
     <div class="menu_bar">
         <a href="index.php" class="logo"><h3>Gacha<span>Fam.</span></h3></a>
         <ul>
-        <li><a href="#" id="profile">Welcome, <span style="color:red"><?php echo ("$username")?></span></a></li>
+            <li>Your credits: <?php echo htmlspecialchars($userCredits, ENT_QUOTES, 'UTF-8'); ?></li>
+            <li><a href="#" id="profile">Welcome, <span style="color:red"><?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?></span></a></li>
+            <li><a href="credit.php">Add Credit</a></li>
             <li><a href="create.php">Create game</a></li>
             <li><a href="cases.html">Cases</a></li>
             <li><a href="cart.html">Cart</a></li>
@@ -41,7 +57,9 @@ try {
                 <h3><?php echo htmlspecialchars($game['game_name']); ?></h3>
                 <p><?php echo htmlspecialchars($game['price']); ?>$</p>
                 <p><?php echo htmlspecialchars($game['game_desc']); ?></p>
+                <p>Credits: <?php echo htmlspecialchars($game['credits']); ?></p>
             </div>
+            </a>
             <?php endforeach; ?>
         </div>
     </section>
